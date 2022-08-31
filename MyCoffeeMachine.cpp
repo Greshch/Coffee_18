d869d69c6b7c7a1097e3f26feedadb79bf8fe339
Greshch <greshchook@gmail.com>
2022-08-31 08:25:34 +0300

    Seters(milk, coffee, water, sugar) were implemented

--- MyCoffeeMachine.cpp
+++ MyCoffeeMachine.cpp
@@ -1,1 +1,49 @@
 #include "MyCoffeeMachine.h"
+
+void MyCoffeeMachine::Load()
+{
+	m_coffee= coffeeTankVol;
+	m_milk = milkTankVol;
+	m_sugar = sugarTankVol;
+	m_water = waterTankVol;
+}
+
+bool MyCoffeeMachine::AddCoffee(uint32_t coffee)
+{
+	if (coffee + m_coffee> coffeeTankVol)
+	{
+		return false;
+	}
+	m_coffee+= coffee;
+	return true;
+}
+
+bool MyCoffeeMachine::AddMilk(uint32_t milk)
+{
+	if (milk + m_milk > milkTankVol)
+	{
+		return false;
+	}
+	m_milk += milk;
+	return true;
+}
+
+bool MyCoffeeMachine::AddSugar(uint32_t sugar)
+{
+	if (sugar + m_sugar > sugarTankVol)
+	{
+		return false;
+	}
+	m_sugar += sugar;
+	return true;
+}
+
+bool MyCoffeeMachine::AddWater(uint32_t water)
+{
+	if (water + m_water > waterTankVol)
+	{
+		return false;
+	}
+	m_water += water;
+	return true;
+}
--- MyCoffeeMachine.h
+++ MyCoffeeMachine.h
@@ -6,6 +6,24 @@
 class MyCoffeeMachine : public ICoffeeMachine,
 						public ICoffeeMachineRecipe
 {
+private:
+	uint32_t m_water = 0;
+	uint32_t m_coffee= 0;
+	uint32_t m_sugar = 0;
+	uint32_t m_milk = 0;
 
+	static const uint32_t one_glass = 125;
+	static const uint32_t waterTankVol = 2500;
+	static const uint32_t milkTankVol = 1000;
+	static const uint32_t coffeeTankVol = 250;
+	static const uint32_t sugarTankVol = 250;
+
+public:
+	MyCoffeeMachine() = default;
+	void Load();
+	bool AddCoffee(uint32_t coffee);
+	bool AddMilk(uint32_t milk);
+	bool AddSugar(uint32_t sugar);
+	bool AddWater(uint32_t water);
 };
 
